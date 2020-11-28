f4d4dbbe732eb824d95fea60a35095cb1eb30d2a
gmujica <gregorymujica1993@gmail.com>
2020-11-28 16:26:54 -0500

    add

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.css'
+
+function App() {
+  const [coins, setCoins] = useState([])
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
+       setCoins(res.data)
+      console.log(res.data)
+    })
+    .catch(error => console.log(error))
+  }, [])
+
+  return (
+    <div className="coin-app">
+      <div className='coin-search'></div>
+      <h1>Search a Currency</h1>
+      <form>
+        <input 
+          type='text'
+          placeholder='Search'
+          className='coin-input'
+        />
+      </form>
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/Coin/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Coin({ name, image, symbol, price, volume }) {
+    return (
+        <div className='coin-container'>
+            <div className='coin-row'>
+                <div className='coin'>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Coin
